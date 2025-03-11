098f2bc58494fc33b26795989dbb414a6c5b0ea1
josealvarez <jolualca-12@hotmail.com>
2025-03-11 16:49:01 -0500

    commit

--- lib/screens/register_dialog_screen.dart
+++ lib/screens/register_dialog_screen.dart
@@ -23,66 +23,92 @@ class RegisterDialogScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) {
-          if (state.user != null && state.lastAction == AuthAction.signUp) {
-            _showSnack(
-              context,
-              'Registro de usuario exitoso',
-              color: Colors.green,
-              icon: Icons.check,
-            );
-            Future.delayed(const Duration(seconds: 1), () {
-              Navigator.pop(context); // cierra el diálogo
-            });
-          } else if (state.errorMessage != null) {
-            _showSnack(
-              context,
-              state.errorMessage!,
-              color: Colors.red,
-              icon: Icons.error,
-            );
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          final bloc = context.read<AuthBloc>();
-          return CustomDialog(
-            title: 'Registrarse',
-            contect: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CustomTextField(
-                  label: 'Email',
-                  icon: Icons.email_outlined,
-                  onChanged: (value) => bloc.add(EmailChanged(value)),
-                  errorText: state.emailError,
-                ),
-                const SizedBox(height: 10),
-                CustomTextField(
-                  label: 'Contraseña',
-                  icon: Icons.lock_outlined,
-                  obscureText: true,
-                  onChanged: (value) => bloc.add(PasswordChanged(value)),
-                  errorText: state.passwordError,
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
-            primaryButtonText: 'Registrar',
-            onPrimaryPressed:
-                state.isFormValid
-                    ? () {
-                      bloc.add(SignUpRequested(state.email, state.password));
-                    }
-                    : null,
-            secondaryButtonText: 'Cancelar',
-            onSecondaryPressed: () => Navigator.pop(context),
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state.user != null && state.lastAction == AuthAction.signUp) {
+          _showSnack(
+            context,
+            'Registro de usuario exitoso',
+            color: Colors.green,
+            icon: Icons.check,
           );
-        },
-      ),
+          Future.delayed(const Duration(seconds: 1), () {
+            Navigator.pop(context); // cierra el diálogo
+          });
+        } else if (state.errorMessage != null) {
+          _showSnack(
+            context,
+            state.errorMessage!,
+            color: Colors.red,
+            icon: Icons.error,
+          );
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        final bloc = context.read<AuthBloc>();
+        return CustomDialog(
+          title: 'Registrarse',
+          contect: _registerDialogContent(context),
+          /* Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CustomTextField(
+                label: 'Email',
+                icon: Icons.email_outlined,
+                onChanged: (value) => bloc.add(EmailChanged(value)),
+                errorText: state.emailError,
+              ),
+              const SizedBox(height: 10),
+              CustomTextField(
+                label: 'Contraseña',
+                icon: Icons.lock_outlined,
+                obscureText: true,
+                onChanged: (value) => bloc.add(PasswordChanged(value)),
+                errorText: state.passwordError,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),*/
+          primaryButtonText: 'Registrar',
+          onPrimaryPressed:
+              state.isFormValid
+                  ? () {
+                    bloc.add(SignUpRequested(state.email, state.password));
+                  }
+                  : null,
+          secondaryButtonText: 'Cancelar',
+          onSecondaryPressed: () => Navigator.pop(context),
+        );
+      },
+    );
+  }
+
+  Widget _registerDialogContent(BuildContext context) {
+    final authBloc = context.read<AuthBloc>();
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomTextField(
+              label: 'Email',
+              icon: Icons.email_outlined,
+              onChanged: (value) => authBloc.add(EmailChanged(value)),
+              errorText: state.emailError,
+            ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              label: 'Contraseña',
+              icon: Icons.lock_outlined,
+              obscureText: true,
+              onChanged: (value) => authBloc.add(PasswordChanged(value)),
+              errorText: state.passwordError,
+            ),
+            const SizedBox(height: 20),
+          ],
+        );
+      },
     );
   }
 }
